14b09975fc1df42e8142ec052708eeb1640fdc0d
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2021-11-16 22:15:20 -0800

    new bash

---
+++ slurm/berkeley_16_0.0001_parcel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --begin=now
+#SBATCH --job-name=tcm
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nathanjo@law.stanford.edu
+#SBATCH --partition=owners
+#SBATCH --time=1:00:00
+#SBATCH --mem=4GB 
+
+cd ../
+
+singularity exec $GROUP_HOME/singularity/cafo_permit+tcm_10-21-21.sif python run_algorithm1.py --dataset "../all_buildings/data/input/berkeley/adu_footprints_manualadd_tcm.geojson" --num_clusters 16 --buffer 0.0001 --output_dir ../all_buildings/data/output/berkeley/adu_16_0.0001_parcel/ --algorithm kl --parcel_type parcel
+
