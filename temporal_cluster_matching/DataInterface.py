15ab71d1b5751823d8fb919811996c9e2f4b2b91
Nathan Jo <nathanjo@DN0a22487d.SUNet>
2021-12-02 14:04:31 -0800

    pls

--- temporal_cluster_matching/DataInterface.py
+++ temporal_cluster_matching/DataInterface.py
@@ -57,6 +57,7 @@ def get_mask_and_bounding_geoms(geom, parcel_geom, buffer):
     else:
         bounding_shape = footprint_shape.envelope.buffer(buffer).envelope
 
+    print(footprint_shape.envelope.buffer(0.0003).envelope)
     superres_shape = footprint_shape.envelope.buffer(0.0003).envelope
     print(superres_shape)
 
