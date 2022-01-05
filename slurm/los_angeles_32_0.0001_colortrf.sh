84ff9703d3ca35ae56d88501cdaa2671667cc7a4
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2022-01-05 15:47:56 -0800

    change slurm

--- slurm/los_angeles_32_0.0001_colortrf.sh
+++ slurm/los_angeles_32_0.0001_colortrf.sh
@@ -9,5 +9,5 @@
 
 cd ../
 
-singularity exec $GROUP_HOME/singularity/cafo_permit+tcm_10-21-21.sif python run_algorithm1.py --dataset "../all_buildings/data/input/los_angeles/tcm.geojson" --num_clusters 32 --buffer 0.0001 --output_dir ../all_buildings/data/output/los_angeles/32_0.0001_colortrf --algorithm kl --method colortrf
+singularity exec $GROUP_HOME/singularity/cafo_permit+tcm_10-21-21.sif python run_algorithm1.py --dataset "../all_buildings/data/input/los_angeles/tcm.geojson" --num_clusters 32 --buffer 0.0001 --output_dir ../all_buildings/data/output/los_angeles/32_0.0001_colortrf_within --algorithm kl --method colortrf
 
