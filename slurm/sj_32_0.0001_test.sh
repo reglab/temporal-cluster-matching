8f5566279f838db96021def223c739d1034a57ef
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2022-03-21 13:17:54 -0700

    try 2

--- slurm/sj_32_0.0001_test.sh
+++ slurm/sj_32_0.0001_test.sh
@@ -4,11 +4,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nathanjo@law.stanford.edu
 #SBATCH --partition=owners
-#SBATCH --time=2:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8GB 
+#SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4GB 
 
 cd ../
 
-singularity exec $GROUP_HOME/singularity/cafo_permit+tcm_10-21-21.sif python run_algorithm_parallel.py --dataset "../all_buildings/data/input/OSM/san_jose1.geojson" --num_clusters 32 --buffer 0.0001 --output_dir ../all_buildings/data/output/san_jose/32_0.0001
+singularity exec $GROUP_HOME/singularity/cafo_permit+tcm_10-21-21.sif python run_algorithm_parallel.py --dataset "../all_buildings/data/input/OSM/san_jose_test.geojson" --num_clusters 32 --buffer 0.0001 --output_dir ../all_buildings/data/output/san_jose/32_0.0001
 
