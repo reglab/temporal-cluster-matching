7e6fc29cce9f5e990a7e1011c276e471349bb66d
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2022-03-22 22:51:29 -0700

    tht

--- run_algorithm_parallel.py
+++ run_algorithm_parallel.py
@@ -72,7 +72,7 @@ def main():
     if os.path.exists(args.output_dir):
         # see if csv exists within directory
         if os.path.exists(os.path.join(args.output_dir, "results.csv")):
-            results = pd.read_csv(os.path.join(args.output_dir, "results.csv"))
+            results = pd.read_csv(os.path.join(args.output_dir, "results.csv"), delim_whitespace=True)
             index_done = results.iloc[:, 0].tolist()
             index_done = [str(i) for i in index_done]
     else:
