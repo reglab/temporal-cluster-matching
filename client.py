a34979fa6ec18f2b559576f214f9a04199e497f9
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2022-03-22 21:16:46 -0700

    client

---
+++ client.py
@@ -0,0 +1,7 @@
+import t
+manager = t.RtreeManager(address=('', 50000), authkey=b'')
+manager.connect()
+# print(manager.expose())
+result = manager.intersection((-121.9397863207285, 37.36443486181571, -121.9397863207285, 37.36443486181571))
+# print(type(result))
+print(result)
