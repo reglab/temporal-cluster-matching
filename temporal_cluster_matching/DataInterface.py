c6307c4ab673a9dcac7f4f0388ce363cd8b3c739
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2022-03-23 22:08:25 -0700

    hi

--- temporal_cluster_matching/DataInterface.py
+++ temporal_cluster_matching/DataInterface.py
@@ -347,7 +347,7 @@ class NAIPDataLoader(AbstractDataLoader):
                     with rasterio.open(path_to_fn + fn) as f:
                         try:
                             mask_image, _ = rasterio.mask.mask(f, [mask_geom], crop=True, invert=False, pad=False,
-                                                               all_touched=True, filled=False)
+                                                               all_touched=True) # add filled=False is good when we want to print out a complete picture without a black border
                         except Exception as e:
                             print(index)
                             print("Mask image not executed, skipping (year: {})".format(year))
@@ -358,7 +358,7 @@ class NAIPDataLoader(AbstractDataLoader):
                         try:
                             full_image, full_transform = rasterio.mask.mask(f, [bounding_geom], crop=True, invert=False,
                                                                             pad=False,
-                                                                            all_touched=True, filled=False)
+                                                                            all_touched=True)
                         except Exception as e:
                             print(index)
                             print("full image not executed, skipping (year: {})".format(year))
@@ -413,12 +413,6 @@ class NAIPDataLoader(AbstractDataLoader):
                         #     dst.write(full_image_mask[:3, :, :])
                         ### END PRINT
 
-                        with open(f'{index}_old.p', 'wb') as g:
-                            pickle.dump(mask_image, g)
-                        full_image = np.rollaxis(full_image, 0, 3)
-
-                        with open(f'{index}_old_full.p', 'wb') as h:
-                            pickle.dump(full_image, h)
 
                         mask = np.zeros((mask_image.shape[0], mask_image.shape[1]), dtype=np.bool)
                         mask[np.sum(mask_image == 0, axis=2) == 4] = 1
